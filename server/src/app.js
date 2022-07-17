04a14248fa1d342df78e0e2f1ba605f902422057
happi89 <farhaanp99@gmail.com>
2022-07-17 16:37:42 -0400

    deployed server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -12,6 +13,11 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+	res.send('hello to nasa mission control api');
+});
 
 module.exports = app;
